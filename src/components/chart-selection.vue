a8e0b04824e618c7b706ae6905e6da6300767b05
Kong <Rebecca.Kong@ec.gc.ca>
2025-05-14 10:44:22 -0400

    fixing hidden select template for graph 2

--- src/components/chart-selection.vue
+++ src/components/chart-selection.vue
@@ -130,11 +130,13 @@ const seriesNames = computed(() => Object.values(dataStore.headers).slice(1));
 const router = useRouter();
 
 const enableHybrid = computed(() => {
-    return Array.isArray(chartConfig.value.series) && chartConfig.value.series.length >= 2;
+    console.log(chartConfig.value.series)
+    return Array.isArray(chartConfig.value.series) && chartConfig.value.series.length >= 2 && chartType.value != 'pie';
 });
 
 const enableMultiselect = computed(() => {
-    return Array.isArray(chartConfig.value.series) && chartConfig.value.series.length > 2;
+    console.log(Array.isArray(chartConfig.value.series) && chartConfig.value.series.length >= 2)
+    return Array.isArray(chartConfig.value.series) && chartConfig.value.series.length >= 2 && chartType.value != 'pie';
 });
 
 const chartType = ref<string>('');
